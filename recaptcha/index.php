4daea1998cb986bb19b49835b3ed377f913e610d
aneesh <aneesh.nl@gmail.com>
2010-06-20 07:42:56 +0530

    Recaptha Library

---
+++ recaptcha/index.php
@@ -0,0 +1,37 @@
+<html>
+  <body>
+    <form action="" method="post">
+<?php
+
+require_once('recaptchalib.php');
+
+// Get a key from https://www.google.com/recaptcha/admin/create
+$publickey = "6Lcd87oSAAAAAIjpzZC74bCCtSOMVRRiHnmTy2Mb";
+$privatekey = "6Lcd87oSAAAAADrp5eUYCPPjX518FWsr87RfU9L5";
+
+# the response from reCAPTCHA
+$resp = null;
+# the error code from reCAPTCHA, if any
+$error = null;
+
+# was there a reCAPTCHA response?
+if ($_POST["recaptcha_response_field"]) {
+        $resp = recaptcha_check_answer ($privatekey,
+                                        $_SERVER["REMOTE_ADDR"],
+                                        $_POST["recaptcha_challenge_field"],
+                                        $_POST["recaptcha_response_field"]);
+
+        if ($resp->is_valid) {
+                echo "You got it!";
+        } else {
+                # set the error code so that we can display it
+                $error = $resp->error;
+        }
+}
+echo recaptcha_get_html($publickey, $error);
+?>
+    <br/>
+    <input type="submit" value="submit" />
+    </form>
+  </body>
+</html>
