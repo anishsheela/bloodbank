da1ee8ef6922398703c456efc465a1d604117004
aneesh <aneesh.nl@gmail.com>
2010-06-26 22:24:57 +0530

    Added some functions to calculate class file

--- calculate_class.php
+++ calculate_class.php
@@ -3,6 +3,7 @@
 // Connection settings
 require("./cnn.php");
 
+// There is some bugs in this function. Please correct it.
 function calculate_class($regid) {
     // Select row and database
     $sql = 'SELECT * FROM `registration` WHERE Regid = '.$regid.';';
@@ -37,10 +38,45 @@ function calculate_class($regid) {
     }
 }
 
+// Change date format from dd-mm-yyyy to dd/mm/yyyy
 function change_date_format($date){
-
        list($year,$month,$day) = explode("-",$date);
        $newdate = $day."/".$month."/".$year;
        return $newdate;
+}
+
+// Convert age to approximate date of birth.
+function agetodob($age){
+    $birthdate = (date('Y') - $age).'-1-1';
+    $dob = date('Y-m-d H:i:s', strtotime($birthdate));
+    return $dob;
+}
+
+// Function to convert admission year to agge, assuming the person had
+// compleated 18 years at the year of joining
+function admnyeartoage($year) {
+    return (date('Y') - $year + 18);
+}
+
+// Convert admission year directly to date of birth.
+function admnyeartodob($year) {
+    return agetodob(admnyeartoage($year));
+}
+
+// Function that calculates age from date of birth
+function getAge($iTimestamp)
+{
+    // See http://php.net/date for what the first arguments mean.
+    $iDiffYear  = date('Y') - date('Y', $iTimestamp);
+    $iDiffMonth = date('n') - date('n', $iTimestamp);
+    $iDiffDay   = date('j') - date('j', $iTimestamp);
+
+    // If birthday has not happen yet for this year, subtract 1.
+    if ($iDiffMonth < 0 || ($iDiffMonth == 0 && $iDiffDay < 0))
+    {
+        $iDiffYear--;
+    }
+
+    return $iDiffYear;
 }
 ?>
