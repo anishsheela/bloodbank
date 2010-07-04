ce5d17b2817273ea8eca36c60a8e4572e3442f09
Anish A <aneesh.nl@gmail.com>
2010-07-04 20:28:50 +0530

    Minor change

--- adminreg.php
+++ adminreg.php
@@ -23,7 +23,7 @@
 
 <body>
     
-<script type="javascript">
+<script type="text/javascript">
 function validate(form_id,email) {
    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
    var address = document.forms[form_id].elements[email].value;
