c389119bfbd8119844ceef85fa002c97abc1613c
Anish A <aneesh.nl@gmail.com>
2010-07-07 00:40:21 +0530

    New registration details

---
+++ register.php
@@ -0,0 +1,199 @@
+<?php require 'cnn.php';?>
+<html> 
+<head> 
+<!-- Import Google Font - Yanone Kaffeesatz  -->	
+<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css' />
+
+<title>Blood Donor Registration Form</title>
+<LINK href="register_style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+function check() {
+   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+   var address = document.formcheck.email.value;
+   if(reg.test(address) == false) {
+      alert('Invalid Email Address');
+      return false;
+   }
+    if(document.formcheck.name.value=="") {
+        alert("Enter name");
+        document.formcheck.name.focus();
+        return false;
+    }
+    if(document.formcheck.bloodgroup.selectedIndex==0) {
+        alert("Select Your Blood Group");
+        document.formcheck.bloodgroup.focus();
+        return false;
+    }
+    if(document.formcheck.sex.selectedIndex==0) {
+        alert("Select Your Gender");
+        document.formcheck.jumpMenu.focus();
+        return false;
+    }
+    if(document.formcheck.weight.value=="") {
+        alert("Enter Weight");
+        document.formcheck.weight.focus();
+        return false;
+    }
+    if(document.formcheck.phone.value=="") {
+        alert("Enter Contact Phone Number");
+        document.formcheck.phone.focus();
+        return false;
+    }
+    if(document.formcheck.email.value=="") {
+        alert("Enter Email");
+        document.formcheck.email.focus();
+        return false;
+    }
+    if(document.formcheck.password.value=="") {
+        alert("Enter Password");
+        document.formcheck.password.focus();
+        return false;
+    }
+    if(document.formcheck.repass.value=="") {
+        alert("Re-enter The Same Password Given Above");
+        document.formcheck.repass.focus();
+        return false;
+    }
+    if(document.formcheck.repass.value !== document.formcheck.password.value) {
+        alert("Password Mismatch Please Check It");
+        document.formcheck.repass.focus();
+        return false;
+    }
+    if(document.formcheck.address.value=="") {
+        alert("Enter the address");
+        document.formcheck.repass.focus();
+        return false;
+    }
+}
+</script>
+</head> 
+ 
+<body> 
+    <form name="formcheck" id="formcheck" onsubmit="javascript:return check();" action="test.php" method="post">
+		
+		<h1>Blood Bank Registration</h1>
+		<div id="left-side">	
+			
+			<p>Name</p>
+			<input name="name" id="name"type="text" value="" />
+		
+			<p>Date of Birth</p>
+            <select  name="dd" style="width:auto">
+              <?php for($i=1; $i<=31; $i++) { ?>
+              <option value="<?php echo $i;?>"  >
+              <?php  echo $i;?>
+              </option>
+              <?php } ?>
+            </select>
+            <select name="dm" style="width:auto">
+              <?php
+                for($i=1; $i<=12; $i++)
+                { ?>
+              <option> <?php echo  substr(date("F", strtotime("".$i."/1/1") ),0,9) ; ?> </option>
+              <?php } ?>
+            </select>
+            <select name="dy" style="width:auto">
+                <?php
+                for($i=date("Y"); $i>=date("Y")-90; $i--)
+                { ?>
+                <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                <?php } ?>
+            </select>
+
+			<p>Gender &nbsp;&nbsp;&nbsp;Blood Group&nbsp;&nbsp;Weight</p>
+            <select name="sex" style="width:auto">
+                <option value="" >Select</option>
+                <option value="1">Male</option>
+                <option value="0">Female</option>
+            </select>&nbsp;&nbsp;
+            <select name="bloodgroup" style="width:auto">
+                <option value="">Select</option>
+                <?php
+                    $sql = "SELECT DISTINCT BloodGroup FROM bloodgroup;";
+                    $result = mysql_query($sql);
+                    while ($row1 = mysql_fetch_array($result)) {
+                ?>
+                    <option><?php echo $row1["BloodGroup"];?></option>
+                <?php } ?>
+            </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input name="weight" style="width:50px"/>
+		
+            <p>Year &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Branch&nbsp;&nbsp; Batch</p>
+            <select name="admnyear" style="width:auto">
+                <?php for($i = (int)date('Y'); $i > ((int)date('Y') - 20); $i--) {?>
+                <option><?php echo (string)$i;?></option>
+                <?php }?>
+            </select>&nbsp;&nbsp;
+            <select name="branch" style="width:auto">
+                <?php
+                    $sql = "SELECT DISTINCT course FROM course;";
+                    $result = mysql_query($sql);
+                    while ($row1 = mysql_fetch_array($result)) {
+                ?> <option><?php echo $row1["course"];?></option>
+                <?php } ?>
+            </select>&nbsp;&nbsp;
+            <select name="batch" style="width:auto">
+                <?php
+                    $sql = "SELECT DISTINCT Batch FROM batch_list;";
+                    $result = mysql_query($sql);
+                    while ($row1 = mysql_fetch_array($result)) {
+                ?> <option><?php echo $row1["Batch"];?></option>
+                <?php } ?>
+            </select>
+			<p>Date of Last Donation</p>
+            <select  name="ld" style="width:auto">
+              <?php for($i=1; $i<=31; $i++) { ?>
+              <option value="<?php echo $i;?>"  >
+              <?php  echo $i;?>
+              </option>
+              <?php } ?>
+            </select>
+            <select name="lm" style="width:auto">
+              <?php
+                for($i=1; $i<=12; $i++)
+                { ?>
+              <option> <?php echo  substr(date("F", strtotime("".$i."/1/1") ),0,9) ; ?> </option>
+              <?php } ?>
+            </select>
+            <select name="ly" style="width:auto">
+                <?php
+                for($i=date("Y"); $i>=date("Y")-90; $i--)
+                { ?>
+                <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                <?php } ?>
+            </select>
+            <p>District</p>
+            <select name="address3" style="width:auto">
+                <?php
+                    $sql = "SELECT * FROM `district`;";
+                    $result = mysql_query($sql);
+                    while ($row1 = mysql_fetch_array($result)) {
+                ?> <option><?php echo $row1["Name"];?></option>
+                <?php } ?>
+            </select>
+		</div><!--end user-details-->
+		
+		<div id="right-side">		
+			<p>Phone number&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Publish</p>
+			<input type="text" name="phone" value="" style="width:150px;" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="checkbox" name="publish" checked="false" />
+			<p>Email ID (This will be your username for login)</p>
+			<input type="text" name="email" id="email" value=""/>
+            <p>Password</p>
+			<input type="text" name="password" value=""/>
+            <p>Re Enter Password</p>
+			<input type="text" name="repass" value=""/>
+			<p>Address</p>
+            <textarea cols="" rows=""  name="address"></textarea>
+            <p>&nbsp;</p><p>&nbsp;</p>
+            <input type="reset" value="Reset" name="reset" class="reset"/>
+		</div><!-- end user-message -->
+        <div id="submit-button">
+			<input type="submit" value="Submit" name="submit" class="submit" />
+            
+        </div>
+	</form>	
+ 
+</body> 
+</html> 
---
+++ register_action.php
@@ -0,0 +1,71 @@
+<?php 
+
+require 'cnn.php';
+require 'calculate_class.php';
+require 'hash.php'
+
+// Initialize all the values
+$name="";
+$sex="";
+$bgroup = "";
+$weight = 50;
+$phone = "";
+$email = "";
+$password="";
+$district="";
+$address="";
+$last="";
+$publish1=0;
+
+$sql="select * from user ";
+$result=mysql_query($sql,$link);
+
+while($detail=mysql_fetch_array($result,MYSQL_ASSOC)) {
+    @$usr=$detail['UserID'];
+    if($usr == $_POST["email"]) {
+        header("Location: ./index.php?msg=Username already exists,Please login here");
+        die(mysql_error($link));
+    }
+}		
+			
+
+
+if(isset($_POST["email"])){
+    if( trim($_POST["name"]) != "" ) $name = trim($_POST["name"]);
+    $sex = $_POST["sex"];
+    $bgroup = $_POST["bloodgroup"];
+    if( trim($_POST["phone"]) != "" ) $phone = trim($_POST["phone"]);
+    if( trim($_POST["email"]) != "" ) $email = trim($_POST["email"]);
+    $password = $_POST["password"];
+    if( trim($_POST["address2"]) != "" ) $address = trim($_POST["address2"]);
+    if( trim($_POST["address3"]) != "" ) $district = trim($_POST["address3"]);
+    if( trim($_POST["weight"]) != "" ) $weight = trim($_POST["weight"]);
+    if( trim($_POST["publish"]) != "" ) $publish1 = trim($_POST["publish"]);
+    //DOB
+    $dd = $_POST["dd"];
+    $dm = $_POST["dm"];
+    $dy = $_POST["dy"];
+    // Last date of donation.
+    $ld = $_POST["ld"];
+    $lm = $_POST["lm"];
+    $ly = $_POST["ly"];
+
+    if($publish1 == "on")
+        $publish=1;
+
+    $sql="INSERT INTO registration (Name,DOB,Gender,Bloodgroup,Weight,AdmissionYear, Branch, Batch, Designation, ContactNo,Emailid,LastDonation,Publish,District,Post)
+    VALUES ('$name', '$Regdate',$sex,'$bgroup','$weight','$class','$phone','$email','$last', '$publish','$district','$address')";
+    $sql2="INSERT INTO user (UserID, PWD)VALUES ('$email' , '$password')";
+    mysql_query($sql2,$link);
+    $sql3="UPDATE stock SET Stock = Stock + 1  WHERE BGroup  = '$bgroup'";
+    mysql_query($sql3,$link);
+
+    if(!mysql_query($sql,$link))
+            die ('Error' . mysql_error());
+    else {
+        mysql_query($result1,$link);
+        $_SESSION['key1']='$email';
+        header("Location: ./index.php?msg=Please login here");
+    }
+ }
+ ?>
